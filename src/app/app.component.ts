6a4eb620e2c778c5a8c2eb3f400f75511b7a2ad2
Brayan Cataño Giraldo <b.catano@utp.edu.co>
2024-09-20 11:49:50 -0500

    First steps Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from "./components/users/register/register.component";
+import { HeaderComponent } from "./components/home/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegisterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
