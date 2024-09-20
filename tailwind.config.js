09a2a382ff3afd24394340bebd63a959c6b580d3
Brayan Giraldo <b.catano@utp.edu.co>
2024-09-20 15:10:07 -0500

    Update tailwind.config.js
    Added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color1: {
+          DEFAULT: "#22333B", // Gunmetal
+          shade: "#161E22", // Eerie Black
+        },
+        color2: {
+          DEFAULT: "#B8B5AF", // Silver
+          shade: "#868A88", // Battleship Gray
+        },
+        color3: "#5E503F", // Almond
+        color4: {
+          DEFAULT: "#D8C6B2", // Dun
+          shade: "#C6AC8F", // Khaki
+        },
+        color5: {
+          DEFAULT: "#927E67", // Beaver
+          shade: "#5E503F", // Walnut Brown
+        },
+      },
+    },
   },
   plugins: [],
 }
