ed8a0a4fa46465db0a8cdddbed34a80989a6f78f
xbedoya <ximena.bedoya@utp.edu.co>
2024-09-22 22:03:07 -0500

    Login and register components modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from "./components/users/register/register.component";
+import { LoginComponent } from "./components/users/login/login.component";
 import { HeaderComponent } from "./components/home/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent, HeaderComponent],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
