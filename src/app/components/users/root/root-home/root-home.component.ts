1ad492d52e888e860629ac7cc93db4dcfacfe3c2
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-09-25 09:31:09 -0500

    Add new component

---
+++ src/app/components/users/root/root-home/root-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './root-home.component.html',
+  styleUrl: './root-home.component.css'
+})
+export class RootHomeComponent {
+
+}
