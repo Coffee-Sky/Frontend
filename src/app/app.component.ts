a0be1572e33cbd9852aaac3c7ac0c085d6d4287b
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-09-25 12:06:23 -0500

    Add table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RegisterComponent } from "./components/users/register/register.componen
 import { LoginComponent } from "./components/users/login/login.component";
 import { HeaderComponent } from "./components/home/header/header.component";
 import { PageComponent } from "./components/home/page/page.component";
+import { RootHomeComponent } from "./components/users/root/root-home/root-home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent, LoginComponent, HeaderComponent, PageComponent],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent, HeaderComponent, PageComponent, RootHomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
