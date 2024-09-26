5a1187c1b06b5943c6288553a3fd4532570cd736
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-09-26 07:55:26 -0500

    Implement location API

--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -1,7 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EmailValidator, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { threadId } from 'node:worker_threads';
+import { LocationService } from '../../../services/location.service';
+import { HttpClient } from '@angular/common/http';
+
+interface Country {
+  country_name: string;
+  country_short_name: string;
+  country_phone_code: number;
+}
+
+interface State {
+  state_name: string;
+}
+
+interface City {
+  city_name: string;
+}
 
 @Component({
   selector: 'app-register',
@@ -10,7 +26,78 @@ import { threadId } from 'node:worker_threads';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+
+export class RegisterComponent implements OnInit{
+
+  accessToken: string = '';
+
+  countries: Country[] = [];
+  states: State[] = [];
+  cities: City[] = [];
+
+  constructor(private locationService: LocationService) { }
+
+  ngOnInit() {
+    this.locationService.getAccessToken().subscribe(
+      (response) => {
+        this.accessToken = response.auth_token;
+        // console.log('Access Token:', this.accessToken);
+        this.getCountries();
+        // this.getStates('Colombia');
+        // this.getCities('Antioquia');
+      },
+      (error) => {
+        console.error('Error obteniendo el token de acceso:', error);
+      }
+    );
+  }
+
+  getCountries() {
+    this.locationService.getCountries(this.accessToken).subscribe(
+      (response) => {
+        this.countries = response;
+        // console.log(this.countries);
+      },
+      (error) => {
+        console.error('Error obteniendo los países:', error);
+      }
+    );
+  }
+
+  getStates(event: Event) {
+    const selectedCountry = (event.target as HTMLSelectElement).value;
+
+    // console.log('Selected Country:', selectedCountry);
+
+    this.states = [];
+    this.cities = [];
+
+    this.locationService.getStates(this.accessToken, selectedCountry).subscribe(
+      (response) => {
+        this.states = response;
+        // console.log(this.states);
+      },
+      (error) => {
+        console.error('Error obteniendo los estados:', error);
+      }
+    );
+  }
+
+  getCities(event: Event) {
+    const selectedState = (event.target as HTMLSelectElement).value;
+
+    // console.log('Selected Country:', selectedState);
+
+    this.locationService.getCities(this.accessToken, selectedState).subscribe(
+      (response) => {
+        this.cities = response;
+        // console.log(this.cities);
+      },
+      (error) => {
+        console.error('Error obteniendo las ciudades:', error);
+      }
+    );
+  }
 
   registerForm = new FormGroup({
     firstname: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)]),
