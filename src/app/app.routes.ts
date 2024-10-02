d36c35993c2925132500f3d496b0345377cb50bb
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-10-02 17:05:11 -0500

    route protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/users/register/register.component';
 import { LoginComponent } from './components/users/login/login.component';
 import { PageComponent } from './components/home/page/page.component';
+import { authGuard } from './guards/auth.guard';
+import { noAuthGuard } from './guards/no-auth.guard';
 
 export const routes: Routes = [
   { path: '', component: PageComponent }, 
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [noAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [noAuthGuard] },
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
   })
-  export class AppRoutingModule {}
+  export class AppRoutingModule {} 
