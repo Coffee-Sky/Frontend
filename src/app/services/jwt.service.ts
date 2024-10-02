d7f73e5ef1d5e01dacaa13c211306f9824e6c82d
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-10-02 17:05:21 -0500

    Fix jwt

--- src/app/services/jwt.service.ts
+++ src/app/services/jwt.service.ts
@@ -16,17 +16,48 @@ export class JwtService {
 
   constructor() { }
 
-  setToken(token: string): void {
-    localStorage.setItem(this.TOKEN_KEY, token);
+  setToken(token: string): void | null {
+    if (this.isLocalStorageAvailable()) {
+      localStorage.setItem(this.TOKEN_KEY, token);
+    }
+    else {
+      return null;
+    }
   }
 
   getToken(): string | null {
-    return localStorage.getItem(this.TOKEN_KEY);
+    if (this.isLocalStorageAvailable()) {
+      return localStorage.getItem(this.TOKEN_KEY);
+    }
+    return null;
   }
 
   removeToken(): void {
-    localStorage.removeItem(this.TOKEN_KEY);
+    if (this.isLocalStorageAvailable()) {
+      localStorage.removeItem(this.TOKEN_KEY);
+    }
   }
+
+  tokenExistsAndValid(): boolean {
+    const token = this.getToken();
+    if (!token) return false;
+    // Verifica si el token ha expirado (usa la lógica que ya tenías)
+    const decoded: any = jwtDecode(token);
+    const expiration = decoded.exp;
+    const now = Date.now() / 1000;
+
+    return expiration > now;
+  }
+
+  // Método para verificar si localStorage está disponible
+  private isLocalStorageAvailable(): boolean {
+    try {
+      return typeof localStorage !== 'undefined' && localStorage !== null;
+    } catch (e) {
+      return false;
+    }
+  }
+
 
   decodeToken(): token | null {
     const token = this.getToken();
