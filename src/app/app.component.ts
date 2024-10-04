db02ca64ac4afa700724f06e5987e8c9dfc83ccf
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-10-04 10:38:55 -0500

    Implement cloudinary service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from "./components/users/login/login.component";
 import { HeaderComponent } from "./components/home/header/header.component";
 import { PageComponent } from "./components/home/page/page.component";
 import { RootHomeComponent } from "./components/users/root/root-home/root-home.component";
+import { CloudinaryService } from './services/cloudinary.service';
 
 @Component({
   selector: 'app-root',
@@ -15,4 +16,29 @@ import { RootHomeComponent } from "./components/users/root/root-home/root-home.c
 })
 export class AppComponent {
   title = 'frontend';
+
+  // selectedFile: File | null = null;
+
+  // constructor(private cloudinary: CloudinaryService) {}
+
+  //  // Método para manejar la selección del archivo
+  // onFileSelected(event: any) {
+  //   if (event.target.files && event.target.files[0]) {
+  //     this.selectedFile = event.target.files[0];
+  //   }
+  // }
+
+  // // Método para subir la imagen
+  // uploadImage() {
+  //   if (this.selectedFile) {
+  //     this.cloudinary.uploadImage(this.selectedFile).subscribe(
+  //       (response) => {
+  //         console.log('Imagen subida correctamente', response);
+  //       },
+  //       (error) => {
+  //         console.error('Error al subir la imagen', error);
+  //       }
+  //     );
+  //   }
+  // }
 }
