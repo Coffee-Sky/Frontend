52d055a2a6994ad9ee59f5230674eab955fc14e3
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-10-08 14:43:13 -0500

    Add new guards

---
+++ src/app/guards/root.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { JwtService } from '../services/jwt.service';
+
+export const rootGuard: CanActivateFn = (route, state) => {
+  const jwtService = inject(JwtService);  // Usamos `inject` para acceder a los servicios
+  const router = inject(Router);
+
+  if (jwtService.tokenExistsAndValid()) {
+    const decodeToken = jwtService.decodeToken();
+    if (decodeToken && decodeToken.role === 'ROLE_ROOT') {
+      return true;
+    }
+  }
+  router.navigate(['/login']);  // Redirige al login si el token no es válido o no existe
+  return false;
+};
