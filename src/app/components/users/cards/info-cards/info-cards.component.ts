b1925c919059d9288cb6cdfd8fc43a6f39ceac40
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-10-30 15:44:36 -0500

    Implement view details and edit card

--- src/app/components/users/cards/info-cards/info-cards.component.ts
+++ src/app/components/users/cards/info-cards/info-cards.component.ts
@@ -7,6 +7,41 @@ import { ModalService } from '../../../../services/modal.service';
 import { DeleteCardComponent } from '../delete-card/delete-card.component';
 import { ApiService } from '../../../../services/api.service';
 
+interface Card {
+  cardId: number;
+  cardHolderId: number;
+  cardHolderFirstName: string;
+  cardHolderSecondName: string;
+  cardHolderFirstLastName: string;
+  cardHolderSecondLastName: string;
+  cardHolderPhoneNumber: string;
+  type: string;
+  cardNumber: string;
+  expirationDate: string;
+  cvv: string;
+  balance: number;
+  billingInfoId: number;
+}
+
+interface BillingInfo {
+  billingInfoID: number;
+  userId: number;
+  country: string;
+  state: string;
+  city: string;
+  zone: string;
+  streetName: string;
+  streetNumberOne: string;
+  streetNumberTwo: string;
+  streetType: string;
+  postalCode: string;
+}
+
+interface paymentInfo {
+  card: Card;
+  billingInfo: BillingInfo;
+}
+
 @Component({
   selector: 'app-info-cards',
   standalone: true,
@@ -25,6 +60,42 @@ export class InfoCardsComponent implements OnInit {
 
   streetTypes = ['Autopista', 'Avenida', 'Calle', 'Carrera', 'Circular', 'Circunvalar', 'Diagonal', 'Manzana', 'Pasaje', 'Peatonal', 'Transversal', 'Vereda', 'Vía', 'Zona', 'Otro'];
 
+  paymentInfo: paymentInfo = {
+    card: {
+      cardId: 0,
+      cardHolderId: 0,
+      cardHolderFirstName: '',
+      cardHolderSecondName: '',
+      cardHolderFirstLastName: '',
+      cardHolderSecondLastName: '',
+      cardHolderPhoneNumber: '',
+      type: '',
+      cardNumber: '',
+      expirationDate: '',
+      cvv: '',
+      balance: 0,
+      billingInfoId: 0
+    },
+    billingInfo: {
+      billingInfoID: 0,
+      userId: 0,
+      country: '',
+      state: '',
+      city: '',
+      zone: '',
+      streetName: '',
+      streetNumberOne: '',
+      streetNumberTwo: '',
+      streetType: '',
+      postalCode: ''
+    }
+  };
+
+  typeMap: { [key: string]: string } = {
+    debit: 'Débito',
+    credit: 'Crédito'
+  };
+
   constructor(private fb: FormBuilder, private cdRef: ChangeDetectorRef, private deleteCardService: ModalService, private route: ActivatedRoute, private router: Router, private apiService: ApiService) { }
 
   ngOnInit(): void {
@@ -35,31 +106,67 @@ export class InfoCardsComponent implements OnInit {
 
     this.deleteCardService.$cancel.subscribe((value)=>{this.deleteCard = value})
     this.editCardForm = this.fb.group({
-      type: ['', Validators.required],
-      number: ['', [Validators.required, Validators.pattern(/^\d{16}$/)]],
-      cvv: ['', [Validators.required, Validators.pattern(/^\d{3,4}$/)]],
-      caducityDate: ['', Validators.required],
-      balance: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
-      firstname: ['', [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)]],
-      secondname: ['', Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)],
-      firstlastname: ['', [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)]],
-      secondlastname: ['', Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)],
-      phone: ['', [Validators.required, Validators.pattern(/^\d{7,15}$/)]],
-      country: ['', Validators.required],
-      state: ['', Validators.required],
-      city: ['', Validators.required],
-      neighborhood: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9\sÁÉÍÓÚáéíóúÑñ]+$/)]],
-      streetType: ['', Validators.required],
-      streetName: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9\sÁÉÍÓÚáéíóúÑñ]+$/)]],
-      streetNumber1: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9]{1,5}$/)]],
-      streetNumber2: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9]{1,5}$/)]],
-      postalCode: ['', [Validators.required, Validators.pattern(/^\d{4,10}$/)]],
+      type: [this.paymentInfo.card.type, Validators.required],
+      cardNumber: [this.paymentInfo.card.cardNumber, [Validators.required, Validators.pattern(/^\d{16}$/)]],
+      cvv: [this.paymentInfo.card.cvv, [Validators.required, Validators.pattern(/^\d{3,4}$/)]],
+      expirationDate: [this.paymentInfo.card.expirationDate, Validators.required],
+      balance: [this.paymentInfo.card.balance, [Validators.required, Validators.pattern(/^\d+$/)]],
+      cardHolderFirstName: [this.paymentInfo.card.cardHolderFirstName, [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)]],
+      cardHolderSecondName: [this.paymentInfo.card.cardHolderSecondName, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)],
+      cardHolderFirstLastName: [this.paymentInfo.card.cardHolderFirstLastName, [Validators.required, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)]],
+      cardHolderSecondLastName: [this.paymentInfo.card.cardHolderSecondLastName, Validators.pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/)],
+      cardHolderPhoneNumber: [this.paymentInfo.card.cardHolderPhoneNumber, [Validators.required, Validators.pattern(/^\d{7,15}$/)]],
+      country: [this.paymentInfo.billingInfo.country, Validators.required],
+      state: [this.paymentInfo.billingInfo.state, Validators.required],
+      city: [this.paymentInfo.billingInfo.city, Validators.required],
+      zone: [this.paymentInfo.billingInfo.zone, [Validators.required, Validators.pattern(/^[A-Za-z0-9\sÁÉÍÓÚáéíóúÑñ]+$/)]],
+      streetType: [this.paymentInfo.billingInfo.streetType, Validators.required],
+      streetName: [this.paymentInfo.billingInfo.streetName, [Validators.required, Validators.pattern(/^[A-Za-z0-9\sÁÉÍÓÚáéíóúÑñ]+$/)]],
+      streetNumberOne: [this.paymentInfo.billingInfo.streetNumberOne, [Validators.required, Validators.pattern(/^[A-Za-z0-9]{1,5}$/)]],
+      streetNumberTwo: [this.paymentInfo.billingInfo.streetNumberTwo, [Validators.required, Validators.pattern(/^[A-Za-z0-9]{1,5}$/)]],
+      postalCode: [this.paymentInfo.billingInfo.postalCode, [Validators.required, Validators.pattern(/^\d{4,10}$/)]],
+      cardId: [this.code],
     });
+    this.getCardInfo();
     this.originalValues = this.editCardForm.getRawValue();
   }
 
   getCardInfo() {
-    // this.apiService.getData('')
+    this.apiService.getData('data/get-card-info?cardID='+this.code).subscribe(
+      (response: paymentInfo) => {
+        const { card, billingInfo } = response;
+
+        // Asigna los valores a los controles del formulario
+        this.editCardForm.patchValue({
+          type: card.type,
+          cardNumber: card.cardNumber,
+          cvv: card.cvv,
+          expirationDate: card.expirationDate,
+          balance: card.balance,
+          cardHolderFirstName: card.cardHolderFirstName,
+          cardHolderSecondName: card.cardHolderSecondName,
+          cardHolderFirstLastName: card.cardHolderFirstLastName,
+          cardHolderSecondLastName: card.cardHolderSecondLastName,
+          cardHolderPhoneNumber: card.cardHolderPhoneNumber,
+          country: billingInfo.country,
+          state: billingInfo.state,
+          city: billingInfo.city,
+          zone: billingInfo.zone,
+          streetType: billingInfo.streetType,
+          streetName: billingInfo.streetName,
+          streetNumberOne: billingInfo.streetNumberOne,
+          streetNumberTwo: billingInfo.streetNumberTwo,
+          postalCode: billingInfo.postalCode,
+        });
+        console.log(card);
+        this.originalValues = this.editCardForm.getRawValue();
+      },
+      (error) => {
+        console.error('Error obteniendo la información de la tarjeta:', error);
+        window.alert('Error obteniendo la información de la tarjeta. Intente de nuevo más tarde.');
+        window.location.href = '/cards';
+      }
+    );
   }
 
   toggleEdit() {
@@ -75,6 +182,17 @@ export class InfoCardsComponent implements OnInit {
     if (this.editCardForm.valid) {
       console.log(this.editCardForm.value);
       this.isEditing = false;
+
+      this.apiService.putData('update/edit-card', this.editCardForm.value).subscribe(
+        (response) => {
+          console.log(response);
+          window.alert('Tarjeta actualizada correctamente.');
+        },
+        (error) => {
+          console.error('Error actualizando la tarjeta:', error);
+          window.alert('Error actualizando la tarjeta. Intente de nuevo más tarde.');
+        }
+      );
     } else {
       console.log('Formulario invalido');
     }
