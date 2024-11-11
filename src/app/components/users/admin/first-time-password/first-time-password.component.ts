5d75ca7a1d0cd49a7f4acf083fcabe6b3e003978
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-11-11 17:32:16 -0500

    Implement first time password Admins

---
+++ src/app/components/users/admin/first-time-password/first-time-password.component.ts
@@ -0,0 +1,75 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ModalService } from '../../../../services/modal.service';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../../../../services/api.service';
+import { JwtService } from '../../../../services/jwt.service'; 
+
+@Component({
+  selector: 'app-first-time-password',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './first-time-password.component.html',
+  styleUrl: './first-time-password.component.css'
+})
+export class FirstTimePasswordComponent {
+
+  changePassword = new FormGroup({
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&/<>+-])[A-Za-z\d@$!%*?&/<>+-]{8,20}$/)]),
+  })
+
+  constructor(private firstTimePassword: ModalService, private apiService: ApiService, private jwtService: JwtService){}
+
+  save(){
+    if(this.changePassword.valid){
+      this.submitInfo();
+    }
+    else{
+      console.error('Formulario invalido');
+      window.alert('Ingrese la información correctamente.');
+    }
+  }
+  
+  submitInfo() {
+    // this.apiService.postData("auth/login", this.changePassword.value).subscribe(
+    //   (response) => {
+    //     this.token = response['token'];
+    //     if(this.jwtService.verifyIdRole(this.token)){
+    //       this.close();
+    //       this.passwordService.$password.emit(true);
+    //     }
+    //     else{
+    //       window.alert('No tiene permisos para acceder. Vuelva a iniciar sesión.');
+    //       this.jwtService.removeToken();
+    //       window.location.reload();
+    //     }
+    //   },
+    //   (error) => {
+    //     console.error('Error iniciando sesión:', error);
+    //     window.alert('Credenciales incorrectas.');
+    //   }
+    // );}
+    const data = {
+      userID: this.jwtService.decodeToken()?.sub,
+      password: this.changePassword.value.password
+    }
+    this.apiService.putData('update/change-password', data).subscribe(
+      (response) => {
+        // console.log(response);
+        window.alert('Contraseña actualizada');
+        this.close();
+      },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
+
+  close(){
+    if(this.jwtService.decodeToken()?.role === 'ROLE_ADMIN'){
+      window.location.href = '/profile';
+    }
+    this.firstTimePassword.$firstTimePassword.emit(false);
+  }
+
+}
