9fdd1ddd6826ecd41db36b501bfe7000a40474cf
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2024-11-22 08:28:22 -0500

    Implement cancel purchase

--- src/app/components/users/purchase-history/purchase-info/purchase-info.component.ts
+++ src/app/components/users/purchase-history/purchase-info/purchase-info.component.ts
@@ -3,16 +3,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { HeaderComponent } from '../../../home/header/header.component';
 
-interface CartFlight {
-  flightID: number;
-  tripType: string;
-  origin: string;
-  destiny: string;
-  departure: string;
-  passengers: number;
-  selectedClass: 'economy' | 'firstClass';
-  price: number;
-}
+import 'sweetalert2/src/sweetalert2.scss';
+import Swal from 'sweetalert2';
+import { ApiService } from '../../../../services/api.service';
+import { JwtService } from '../../../../services/jwt.service';
 
 interface Flight {
   flightId: number;
@@ -78,25 +72,7 @@ export class PurchaseInfoComponent implements OnInit{
 
   displayedPassengers: Passenger[] = [];
 
-  flights: CartFlight[][] = [
-    [
-      {flightID: 1, tripType: 'roundtrip', origin: 'Pereira', destiny: 'Miami', departure: '2024-12-06T14:00:00', passengers: 1, selectedClass: 'economy', price: 200},
-      {flightID: 2, tripType: 'roundtrip', origin: 'Miami', destiny: 'Pereira', departure: '2024-12-06T14:00:00', passengers: 1, selectedClass: 'firstClass', price: 200},
-    ],
-  ]
-
-  flights2: CartFlight[][] = [
-    [
-      {flightID: 3, tripType: 'oneway', origin: 'Pereira', destiny: 'Miami', departure: '2024-12-06T14:00:00', passengers: 3, selectedClass: 'firstClass', price: 400},
-    ],
-  ]
-
-  passengers = [
-    {firstname: 'Juan', secondname: '', firstlastname: 'Perez', secondlastname: '', genderID: 1, identificationtype: 1, identificationnumber: '123456789', bornDate: '2004-06-06', bornCountry: 'Colombia', email: 'juan@gmail.com'},
-    {firstname: 'Chil', secondname: '', firstlastname: 'Perez', secondlastname: '', genderID: 2, identificationtype: 1, identificationnumber: '123456700', bornDate: '2014-12-06', bornCountry: 'Colombia', email: 'chil@gmail.com'},
-  ]
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private apiService: ApiService, private jwtService: JwtService) { }
 
   ngOnInit(): void {
     // Filtrar el arreglo purchases para obtener la compra con el flightId
@@ -125,8 +101,57 @@ export class PurchaseInfoComponent implements OnInit{
     window.location.reload();
   }
 
-  cancelPurchase(flightID: number) {
-    console.log('Cancelar compra de vuelo con ID: ', flightID);
+  cancelPurchase() {
+
+    const dataCancel = {
+      clientId: this.jwtService.getCode(),
+      originPurchaseId: this.selectedPurchase.reservations[0].purchaseId,
+      isRoundTrip: this.selectedPurchase.isRoundTrip,
+      returnPurchaseId: this.selectedPurchase.reservations[1] ? this.selectedPurchase.reservations[1].purchaseId : null
+    };
+
+    Swal.fire({
+      icon: "question",
+      title: "Cancelar compra",
+      text: "¿Está seguro que desea cancelar la compra de los tiquetes?",
+      showConfirmButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Sí",
+      cancelButtonText: "No",
+      confirmButtonColor: "#0F766E",
+      cancelButtonColor: "#EF4444"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.apiService.putData('cart/cancel-purchase', dataCancel).subscribe(
+          (response) => {
+            console.log('Respuesta de cancelación:', response);
+            Swal.fire({
+              icon: "success",
+              title: "Cancelar compra",
+              text: "Se ha cancelado la compra exitosamente.",
+              showConfirmButton: false,
+              timer: 2500,
+              timerProgressBar: true
+            }).then(() => {
+              this.goBack();
+            });
+          },
+          (error) => {
+            console.error('Error cancelando la compra:', error);
+            Swal.fire({
+              icon: "error",
+              title: "Cancelar compra",
+              text: "Se ha producido un error cancelando la compra. Vuelve a intentarlo.",
+              showConfirmButton: false,
+              timer: 2500,
+              timerProgressBar: true
+            }).then(() => {
+              this.goBack();
+            })
+          }
+        );
+      }
+    })
   }
 
 }
